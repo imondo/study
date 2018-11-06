6c072872d08763f53530e793893a5bc962d56cdb
Mondo <568197478@qq.com>
2018-11-06 17:36:04 +0800

    导出修改

--- excel/html/index.js
+++ excel/html/index.js
@@ -1,10 +1,104 @@
-function doit(type, fn, dl) {
-	var elt = document.getElementById('tableBox');
-  var wb = XLSX.utils.table_to_book(elt); // , {sheet:"Sheet JS"}
-  console.log(wb);
-  wb.Sheets.Sheet1.A1.s = { fill: {patternType: "none",fgColor: {rgb: "FF000000"},bgColor: {rgb: "00000000"}} }
+function doit() {
+  var elt = document.getElementById('tableBox'); 
   
-	return dl ?
-  XLSXStyle.write(wb, {bookType:type, bookSST:true, type: 'base64'}) :
-  XLSXStyle.writeFile(wb, fn || ('test.' + (type || 'xlsx')));
+  exportExl(elt);
+}
+
+/**
+ * 
+ * @param {Object} dom table的dom元素
+ * @param {Object} name 导出的表名
+ * @param {Object} type 导出的类型
+ * 
+ */
+function exportExl(dom, name = '导出数据', type) {
+	var wb = XLSX.utils.table_to_book(dom, {sheet: "Sheet JS"});
+	var wopts = {
+    bookType: 'xlsx',
+    bookSST: true,
+    type: 'binary',
+    cellStyles: true
+	};
+
+	setExlStyle(wb['Sheets']['Sheet JS']);
+	
+	let tmpDown = new Blob(
+		[
+			this.s2ab(
+				STYLEXLSX.write(
+					wb,
+					{
+						bookType: type == undefined ? 'xlsx' : type,
+						bookSST: false,
+						type: 'binary'
+					} //这里的数据是用来定义导出的格式类型
+				)
+			)
+		],
+		{
+			type: ''
+		}
+	);
+	saveAs(
+		tmpDown,
+		`${name}` +
+			'.' +
+			(wopts.bookType == 'biff2' ? 'xls' : wopts.bookType)
+	);	
+}
+
+function saveAs(obj, fileName) {
+	let tmpa = document.createElement('a');
+	tmpa.download = fileName || '下载';
+	tmpa.href = URL.createObjectURL(obj);
+	tmpa.click();
+	setTimeout(function() {
+		URL.revokeObjectURL(obj);
+	}, 100);
+}	
+
+
+function s2ab (s){
+	if (typeof ArrayBuffer !== 'undefined') {
+		var buf = new ArrayBuffer(s.length);
+		var view = new Uint8Array(buf);
+		for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xff;
+		return buf;
+	} else {
+		var buf = new Array(s.length);
+		for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xff;
+		return buf;
+	}
+};
+
+function setExlStyle (data) {  
+	
+  let borderAll = {  //单元格外侧框线
+    top: {
+      style: 'thin'
+    },
+    bottom: {
+      style: 'thin'
+    },
+    left: {
+      style: 'thin'
+    },
+    right: {
+      style: 'thin'
+    }
+  }; 
+	data['!cols'] = [];
+	for (let key in data) {
+		if (data[key] instanceof Object) {
+			data[key].s = {
+				border: borderAll,
+				alignment: {
+					horizontal: 'center'   //水平居中对其
+				},
+				numFmt: 0
+			}
+			data['!cols'].push({wpx: 170});
+		}
+	}
+	return data;
 }
--- excel/json/index.js
+++ excel/json/index.js
@@ -1,102 +1,133 @@
-function saveAs(obj, fileName) { //当然可以自定义简单的下载文件实现方式 
-  var tmpa = document.createElement("a");
-  tmpa.download = fileName || "下载";
-  tmpa.href = URL.createObjectURL(obj); //绑定a标签
-  tmpa.click(); //模拟点击实现下载
-  setTimeout(function () { //延时释放
-    URL.revokeObjectURL(obj); //用URL.revokeObjectURL()来释放这个object URL
-  }, 100);
-}
-var jsono = [{ //测试数据
-  "id": 1, //A
-  "合并的列头1": "数据11", //B
-  "合并的列头2": "数据12", //C
-  "合并的列头3": "数据13", //D
-  "合并的列头4": "数据14", //E
-}, {
-  "id": 2,
-  "合并的列头1": "数据21",
-  "合并的列头2": "数据22",
-  "合并的列头3": "数据23",
-  "合并的列头4": "数据24",
-}, {
-  "id": 3,
-  "合并的列头1": "数据21",
-  "合并的列头2": "数据22",
-  "合并的列头3": "数据23",
-  "合并的列头4": "数据24",
-}, {
-  "id": 4,
-  "合并的列头1": "数据21",
-  "合并的列头2": "数据22",
-  "合并的列头3": "数据23",
-  "合并的列头4": "数据24",
-}]; //....
-const wopts = {
-  bookType: 'xlsx',
-  bookSST: true,
-  type: 'binary'
-}; //这里的数据是用来定义导出的格式类型 
-function downloadExl(data, type) {
-  var wb = {
-    SheetNames: ['Sheet1'],
-    Sheets: {},
-    Props: {}
-  };
-  //wb.Sheets['Sheet1'] = XLSX.utils.json_to_sheet(data);//通过json_to_sheet转成单页(Sheet)数据
-  data = XLSX.utils.json_to_sheet(data);
-
-  data["B1"] = {
-    t: "s",
-    v: "合并"
-  };
-  data["D1"] = {
-    t: "s",
-    v: "合并2"
-  }
-  data["!merges"] = [{ //合并第一行数据[B1,C1,D1,E1]
-    s: { //s为开始
-      c: 1, //开始列
-      r: 0 //开始取值范围
-    },
-    e: { //e结束
-      c: 2, //结束列
-      r: 0 //结束范围
-    }
-  }, { //合并第一行数据[B1,C1,D1,E1]
-    s: { //s为开始
-      c: 3, //开始列
-      r: 5 //开始取值范围
-    },
-    e: { //e结束
-      c: 4, //结束列
-      r: 5 //结束范围
-    }
-  }];
-   data["B1"].s = {
-     font: {
-       sz: 18,
-       bold: true
-     },
-     alignment: {
-       horizontal: 'center'
-     }
-   }; //<====设置xlsx单元格样式
-  wb.Sheets['Sheet1'] = data;
-  saveAs(new Blob([s2ab(XLSX.write(wb, wopts))], {
-    type: "application/octet-stream"
-  }), "这里是下载的文件名" + '.' + (wopts.bookType == "biff2" ? "xls" : wopts.bookType));
+const header = {
+  'name': '名称',
+  'age': '年龄',
+  'sex': '性别'
 }
 
-function s2ab(s) {
-  if (typeof ArrayBuffer !== 'undefined') {
-    var buf = new ArrayBuffer(s.length);
-    var view = new Uint8Array(buf);
-    for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
-    return buf;
-  } else {
-    var buf = new Array(s.length);
-    for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
-    return buf;
+const data = [
+  {name: '张一', age: '18', sex: '女'},
+  {name: '张二', age: '23', sex: '男'},
+  {name: '张三', age: '34', sex: '女'},
+  {name: '张四', age: '56', sex: '男'}
+]
+
+/**
+ * @param {Object} header 表格头部
+ * @param {Array} body 表格数据
+ * @param {String} title 表格导出名称
+ * @param {Object} merge 表格表头配置
+ * @param {Object} style 表格样式配置
+ */
+function exportsEXCL() {
+
+  this.downLoad = ({header = {}, body = [], title = 'excel', style = {}, merge = {}}) => {
+    let _merge = Object.assign({depth: 2, group: []}, merge);
+    console.log(_merge);
+    const styleCell = this.setBorderStyle();
+    const _headers = Object.values(header)
+    .map((v, i) => Object.assign({}, {v, position: String.fromCharCode(65 + i) + 1}))
+    .reduce((prev, next) => Object.assign({}, prev, {[next.position]: {v: next.v, s: styleCell}}), {});
+    console.log(_headers);
+    const _body = body
+      .map((v, i) => Object.keys(header).map((k, j) => Object.assign({}, { v: v[k], position: String.fromCharCode(65 + j) + (i + 2) })))
+      .reduce((prev, next) => prev.concat(next))
+      .reduce((prev, next) => Object.assign({}, prev, {[next.position]: {v: next.v, s: styleCell}}), {});
+
+    const output = Object.assign({}, _headers, _body);
+
+    const outputPos = Object.keys(output);
+
+    const ref = outputPos[0] + ':' + outputPos[outputPos.length - 1];
+
+    const wb = {
+      SheetNames: ['mySheet'],
+      Sheets: {
+        'mySheet': Object.assign({}, output, { '!ref': ref })
+      }
+    };
+
+    this.setCustStyle(wb, style);
+    debugger
+    this.save(wb, `${title}.xlsx`);
+  };
+
+  this.setCustStyle = (wb, style) => {
+    
   }
+
+  this.setBorderStyle = () => {
+    const borderAll = {
+      top: {
+        style: 'thin'
+      },
+      bottom: {
+        style: 'thin'
+      },
+      left: {
+        style: 'thin'
+      },
+      right: {
+        style: 'thin'
+      }
+    }; 
+    // data[key].s = {
+    //   border: borderAll,
+    //   alignment: {
+    //     horizontal: 'center'
+    //   }
+    // }
+    return {border: borderAll}
+  };
+
+  this.save = (wb, fileName) => {
+    let wopts = {
+      bookType: 'xlsx',
+      bookSST: false,
+      type: 'binary'
+    }
+    let xw = XLSX.write(wb, wopts);
+    let obj = new Blob(
+      [
+        this.s2ab(xw)
+      ],
+      {
+        type: ''
+      }
+    );
+    let elem = document.createElement('a');
+    elem.download = fileName || '下载';
+    elem.href = URL.createObjectURL(obj);
+    elem.click();
+    setTimeout(function() {
+      URL.revokeObjectURL(obj);
+    }, 100);
+  };
+
+  this.s2ab = s => {
+    if (typeof ArrayBuffer !== 'undefined') {
+      var buf = new ArrayBuffer(s.length);
+      var view = new Uint8Array(buf);
+      for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xff;
+      return buf;
+    } else {
+      var buf = new Array(s.length);
+      for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xff;
+      return buf;
+    }
+  };
+}
+
+
+const doit = new exportsEXCL();
+
+function downLoad() {
+  doit.downLoad({
+    header: header,
+    body: data, 
+    merge: {
+      group: [
+        {startName: 'name', text: '合并', cell: 2}
+      ]
+    }
+  });
 }
