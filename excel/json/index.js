7aed6b3733d25667a82f6391ca9308bac031337a
Mondo <568197478@qq.com>
2018-11-07 20:00:58 +0800

    导出表格

--- excel/json/index.js
+++ excel/json/index.js
@@ -1,59 +1,168 @@
-const header = {
-  'name': '名称',
-  'age': '年龄',
-  'sex': '性别'
-}
+const header = [
+  { name: '名称' },
+  { age: '年龄' },
+  { sex: '性别' },
+  { work: '经验' },
+  { edu: '学历' }
+];
 
 const data = [
-  {name: '张一', age: '18', sex: '女'},
-  {name: '张二', age: '23', sex: '男'},
-  {name: '张三', age: '34', sex: '女'},
-  {name: '张四', age: '56', sex: '男'}
-]
+  { name: '张一', age: '18', sex: '女', work: 2, edu: '本科' },
+  { name: '张二', age: '23', sex: '男', work: 5, edu: '博士' },
+  { name: '张三', age: '34', sex: '女', work: 2, edu: '大专' },
+  { name: '张四', age: '56', sex: '男', work: 6, edu: '本科' }
+];
 
 /**
- * @param {Object} header 表格头部
+ * @param {Array} header 表格头部
  * @param {Array} body 表格数据
  * @param {String} title 表格导出名称
- * @param {Object} merge 表格表头配置
- * @param {Object} style 表格样式配置
+ * @param {Boolean} hasTitle 是否需要表格标题
  */
 function exportsEXCL() {
+  this.downLoad = ({
+    header = [],
+    body = [],
+    title = 'excel',
+    hasTitle = false,
+    style = {},
+    merge = {}
+  }) => {
+    const _merge = Object.assign({ depth: 1, group: [], header: [] }, merge);
 
-  this.downLoad = ({header = {}, body = [], title = 'excel', style = {}, merge = {}}) => {
-    let _merge = Object.assign({depth: 2, group: []}, merge);
-    console.log(_merge);
     const styleCell = this.setBorderStyle();
-    const _headers = Object.values(header)
-    .map((v, i) => Object.assign({}, {v, position: String.fromCharCode(65 + i) + 1}))
-    .reduce((prev, next) => Object.assign({}, prev, {[next.position]: {v: next.v, s: styleCell}}), {});
-    console.log(_headers);
+
+    const _headers = header
+      .map((v, i) => {
+        let key = Object.keys(v);
+        return Object.assign(
+          {},
+          {
+            v: `${v[key[0]]}<key>${key[0]}`,
+            position: String.fromCharCode(65 + i) + (hasTitle ? 1 : 0)
+          }
+        );
+      })
+      .reduce(
+        (prev, next) =>
+          Object.assign({}, prev, {
+            [next.position]: { v: next.v, s: styleCell }
+          }),
+        {}
+      );
     const _body = body
-      .map((v, i) => Object.keys(header).map((k, j) => Object.assign({}, { v: v[k], position: String.fromCharCode(65 + j) + (i + 2) })))
+      .map((v, i) =>
+        header.map((k, j) => {
+          let key = Object.keys(k);
+          return Object.assign(
+            {},
+            {
+              v: v[key[0]],
+              position:
+                String.fromCharCode(65 + j) + (i + (hasTitle ? 2 : 1))
+            }
+          );
+        })
+      )
       .reduce((prev, next) => prev.concat(next))
-      .reduce((prev, next) => Object.assign({}, prev, {[next.position]: {v: next.v, s: styleCell}}), {});
+      .reduce(
+        (prev, next) =>
+          Object.assign({}, prev, {
+            [next.position]: { v: next.v, s: styleCell }
+          }),
+        {}
+      );
 
-    const output = Object.assign({}, _headers, _body);
+    const mergeThead = this.setMergeThead(_headers, _merge, hasTitle, title);
 
-    const outputPos = Object.keys(output);
+    const _merges = this.setTableMerges(header, _headers, _merge, hasTitle);
+
+    const _thead = this.setTableThead(mergeThead);
+
+    const output = Object.assign({}, _thead, _body);
+
+    const outputPos = Object.keys(output).sort();
 
     const ref = outputPos[0] + ':' + outputPos[outputPos.length - 1];
 
     const wb = {
       SheetNames: ['mySheet'],
       Sheets: {
-        'mySheet': Object.assign({}, output, { '!ref': ref })
+        mySheet: Object.assign({}, output, { '!ref': ref, '!merges': _merges })
       }
     };
 
     this.setCustStyle(wb, style);
-    debugger
+    console.log(wb);
     this.save(wb, `${title}.xlsx`);
   };
 
-  this.setCustStyle = (wb, style) => {
-    
+  this.setTableThead = wb => {
+    for (let key in wb) {
+      let i = wb[key].v.indexOf('<key>');
+      if (wb[key].v.includes('<key>')) {
+        wb[key].v = wb[key].v.substr(0, i);
+      }
+    }
+    return wb;
   }
+
+  // 设置合并表头
+  this.setTableMerges = (header, wb, merge, hasTitle) => {
+    let _merges = [];
+    console.log(wb);
+    let len = header.length - 1;
+    if (hasTitle) {
+      let o = {
+        s: {
+          c: 0,
+          r: 0,
+        },
+        e: {
+          c: len,
+          r: 0
+        }
+      }
+      _merges.push(o);
+    }
+    return [..._merges, ...merge.header];
+  };
+
+  // 设置表头
+  this.setMergeThead = (wb, merge, hasTitle, title) => {
+    const borderAll = {
+      top: {
+        style: 'thin'
+      },
+      bottom: {
+        style: 'thin'
+      },
+      left: {
+        style: 'thin'
+      },
+      right: {
+        style: 'thin'
+      }
+    };
+    if (hasTitle) {
+      wb['A1'] = {
+        v: `${title}`,
+        s: {
+          border: borderAll,
+          font: {
+            sz: 18,
+            bold: true
+          },
+          alignment: {
+            horizontal: 'center'
+          }
+        }
+      };
+    }
+    return wb;
+  };
+
+  this.setCustStyle = (wb, style) => {};
 
   this.setBorderStyle = () => {
     const borderAll = {
@@ -69,14 +178,8 @@ function exportsEXCL() {
       right: {
         style: 'thin'
       }
-    }; 
-    // data[key].s = {
-    //   border: borderAll,
-    //   alignment: {
-    //     horizontal: 'center'
-    //   }
-    // }
-    return {border: borderAll}
+    };
+    return { border: borderAll };
   };
 
   this.save = (wb, fileName) => {
@@ -84,16 +187,11 @@ function exportsEXCL() {
       bookType: 'xlsx',
       bookSST: false,
       type: 'binary'
-    }
+    };
     let xw = XLSX.write(wb, wopts);
-    let obj = new Blob(
-      [
-        this.s2ab(xw)
-      ],
-      {
-        type: ''
-      }
-    );
+    let obj = new Blob([this.s2ab(xw)], {
+      type: ''
+    });
     let elem = document.createElement('a');
     elem.download = fileName || '下载';
     elem.href = URL.createObjectURL(obj);
@@ -115,19 +213,19 @@ function exportsEXCL() {
       return buf;
     }
   };
-}
 
+  // 根据val查询Object key
+  this.findKey = (val, obj) => {
+    return Object.keys(obj).find(v => obj[v] === val);
+  };
+}
 
 const doit = new exportsEXCL();
 
 function downLoad() {
   doit.downLoad({
     header: header,
-    body: data, 
-    merge: {
-      group: [
-        {startName: 'name', text: '合并', cell: 2}
-      ]
-    }
+    body: data,
+    hasTitle: true
   });
 }
