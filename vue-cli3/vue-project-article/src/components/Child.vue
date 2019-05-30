7c824b4b2bf2b5f822d913028da4cf29bbfee1ac
Mondo <568197478@qq.com>
2019-05-30 11:09:29 +0800

    vue项目文章总结demo

---
+++ vue-cli3/vue-project-article/src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+    我是子组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
