4beef279c134d2f197a34d4bbcaf6ede67b8ff28
Mondo <568197478@qq.com>
2020-02-22 11:32:29 +0800

    feat: redux

--- react-demo/react-test/src/views/index.js
+++ react-demo/react-test/src/views/index.js
@@ -3,3 +3,4 @@ export { default as List } from "./List";
 export { default as ListC } from "./ListC";
 export { default as User } from "./User";
 export { default as Hooks } from "./Hooks";
+export { default as ReduxPage } from "./Redux";
