8c94f321c91a8ff7b5b7d6a63e76ce710d039a8a
Mondo <568197478@qq.com>
2020-03-03 15:58:54 +0800

    feat: react 事件处理

--- react-demo/react-test/src/views/Page.js
+++ react-demo/react-test/src/views/Page.js
@@ -5,13 +5,21 @@ import { GoBack } from './../components'
 class Page extends React.Component {
     constructor() {
         super(...arguments)
+        this.state = {
+          name: ''
+        }
     }
-
+    handleChange(event) {
+      this.setState({
+        name: event.target.value
+      })
+    }
     render() {
         console.log(this.props);
         return (
           <div>
             <h1>这是page页面</h1>
+            <input value={this.state.name} onChange={this.handleChange.bind(this)}></input>
             <GoBack />
           </div>
         );
